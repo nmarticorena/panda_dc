d27fbf456eba79f4b1a36f60c9a2024458327bdb
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-07-21 13:50:06 +1000

    add inquirer py to confirm name of the current gripper eef config

--- scripts/get_gripper.py
+++ scripts/get_gripper.py
@@ -5,6 +5,7 @@ Script to check the parameters of the gripper
 import panda_desk
 import trio
 import json
+from InquirerPy import inquirer
 
 
 desk = panda_desk.Desk("172.16.0.2")
@@ -13,6 +14,10 @@ trio.run(desk.login,"franka","franka123")
 trio.run(desk.take_control, True)
 trio.run(desk.activate_fci)
 
+config_name = inquirer.text(message="Enter the name of the configuration file (without .json):").execute()
+
 params = trio.run(desk.get_eef_parameters)
-with open("config/umi_gripper.json", "w") as f:
+with open(f"config/{config_name}.json", "w") as f:
     json.dump(params, f, indent = 4)
+
+print("Please commit this configs if you want to use it in the future")
